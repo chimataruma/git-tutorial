106e0e63766bd7a494bf8714ea0142c930b9f87e
j2016 <j2016@git.epi.it.matsue-ct.jp>
2023-11-30 16:06:57 +0900

    re

--- kadai.rb
+++ kadai.rb
@@ -3,12 +3,12 @@ while i <=30
     
     if i%15==0
         puts "FizzBuzz!"
-    elsif i%7==0
-        puts "git!"  
     elsif i%3==0
-        puts "Fizz!"
-    elsif i%5==0 
+        puts "Fizz!"  
+    elsif i%5==0
         puts "Buzz!"
+    elsif i%7==0 
+        puts "git!"
     else
         puts i
     end
